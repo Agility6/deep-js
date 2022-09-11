868c55ecc452bd850e434be9f4cf295c6cf0489f
Agility6 <309645035@qq.com>
2022-09-11 23:50:37 +0800

    update WeakSet

--- ES6/WeakSet.js
+++ ES6/WeakSet.js
@@ -7,6 +7,8 @@
 /**
  * WeakSet的应用场景
  */
+
+// 不允许类中传入this
 const personSet = new WeakSet()
 class Person {
   constructor() {
@@ -24,4 +26,31 @@ const p = new Person()
 p.running() // running Person {}
 
 const p2 = new Person()
-p2.running.call({name: "Ag"}) // No~
+// p2.running.call({name: "Ag"}) // No~
+
+
+// 检测循环引用 --> mdn
+function execRecursively(fn, subject, _refs = new WeakSet()) {
+  // 避免递归
+  if(_refs.has(subject)) {
+    return
+  }
+
+  fn(subject)
+  if(typeof subject === 'object') {
+    _refs.add(subject)
+    for(const key in subject) {
+      execRecursively(fn, subject[key], _refs)
+    }
+  }
+}
+
+const foo = {
+  foo: "Foo",
+  bar: {
+    bar: "Bar"
+  }
+}
+
+foo.bar.baz = foo // 循环引用
+execRecursively((obj) => console.log(obj), foo)
