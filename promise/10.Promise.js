48d9bb17d0840d6ad6e4039548d9eef583fd3b11
Agility6 <agility6@163.com>
2022-10-28 01:17:56 +0800

    update

--- promise/10.Promise.js
+++ promise/10.Promise.js
@@ -249,31 +249,3 @@ class AgilityPromise {
     })
   }
 }
-
-
-const p1 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    // resolve('okk')
-    reject('p1 err')
-  }, 1000)
-})
-
-const p2 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('p2 okk')
-    // reject('p2 err')
-  }, 2000)
-})
-
-const p3 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    // resolve('p3 okk')
-    reject('p3 err')
-  }, 3000)
-})
-
-Promise.any([p1,p2,p3]).then(res => {
-  console.log(res)
-}).catch(err => {
-  console.log(err)
-})
